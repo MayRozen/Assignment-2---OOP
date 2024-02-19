23bd61f108636f274012dd478307a1c76610b9dc
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-19 09:55:04 +0200

    Update Posts.py

--- Posts.py
+++ Posts.py
@@ -1,5 +1,46 @@
-class Posts:
+from enum import Enum
+from abc import ABC, abstractmethod
 
-    def __init__(self, type, content):
-        self.type = type
-        self.content = content
+from ImagePost import ImagePost
+from SalePost import SalePost
+from TextPost import TextPost
+
+
+class PostType(Enum):
+    TEXTPOST = "textPost"
+    IMAGEPOST = "imagePost"
+    SALEPOST = "salePost"
+
+
+class Posts:
+    like_count = 0
+    comments = []
+    owner = None
+
+    def create_post(self, post_type, owner, *content):  # Factory
+        if post_type == post_type.TEXTPOST:
+            text = content[0]
+            print("Text Post")
+            return TextPost(text)
+        elif post_type == post_type.IMAGEPOST:
+            image = content[0]
+            print("Image Post")
+            return ImagePost(image)
+        elif post_type == post_type.SALEPOST:
+            description = content[0]
+            price = content[1]
+            location = content[2]
+            print("Sale Post")
+            return SalePost(description, price, location)
+        else:
+            raise ValueError("Invalid post type")
+
+    def like(self):  # continue
+        self.like_count += 1
+
+    def comment(self, user, content):
+        self.comments.append(user, content)
+
+    @abstractmethod
+    def print_info(self, user):
+        pass
