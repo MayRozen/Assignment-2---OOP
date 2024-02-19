acee1dfef4d910bd8a2a52ceb0985df44041816c
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-19 09:54:04 +0200

    Update SocialNetwork.py

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -2,18 +2,18 @@ import self
 
 
 class SocialNetwork:
-    __active_network = None #Singleton
+    _active_network = None  # Singleton
     userName = None
     password = None
     check_userName = []
 
     def __new__(cls):
-        if cls.__active_network is None: # If an instance does not exist -> create a new one
-            cls.__active_network = super().__new__(cls) # super -> from Object class
-        return cls.__active_network
+        if cls._active_network is None:  # If an instance does not exist -> create a new one
+            cls._active_network = super().__new__(cls)  # super -> from Object class
+            print(_active_network)
+        return cls._active_network
 
-
-    def sign_up(self, new_userName, new_password:
+    def sign_up(self, new_userName, new_password):
         if new_userName not in self.check_userName:
             self.userName = new_userName
             self.check_userName.append(new_userName)
@@ -21,19 +21,22 @@ class SocialNetwork:
             print("This name is already registered in the system. Choose another name.")
 
         length_pass = len(new_password)
-        if 4<=length_pass<=8:
+        if 4 <= length_pass <= 8:
             self.password = new_password
         else:
             print("This password is invalid. Please change password")
             self.password = input()
 
-    def log_in (self,  userName,  password):
-         self.users(userName, password).isConnected()
+    def log_in(self, new_userName, new_password):
+        if new_userName in self.check_userName:
+            if new_password in self.check_userName:
+                Users.isConnected = True
+                print("The user in")
 
-
-
-    def log_out (self, userName):
-        return False
+    def log_out(self, new_userName):
+        if new_userName in self.check_userName:
+            Users.isConnected = False
+            print("The user out")
 
 
 
