09ee6dacafb4cc517b31e30da59afe17f684e3fc
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-21 23:45:25 +0200

    Update SocialNetwork.py

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,41 +1,35 @@
+import Users
+
 
 class SocialNetwork:
     _active_network = None  # Singleton
-    userName = None
-    password = None
-    check_userName = []
+    check_userName = {}
 
-    def __new__(cls, new_nameNetwork):
+    def __new__(cls, new_name_network):
         if cls._active_network is None:  # If an instance does not exist -> create a new one
             cls._active_network = super().__new__(cls)  # super -> from Object class
-            cls._active_network = new_nameNetwork
-            print(_active_network)
+            cls._active_network = new_name_network
+            print(cls._active_network)
         return cls._active_network
 
-
-    def sign_up(self, new_userName, new_password):
-        if new_userName not in self.check_userName:
-            self.userName = new_userName
-            self.check_userName.append(new_userName)
+    def sign_up(self, new_username, new_password):
+        if new_username not in self.check_userName:
+            if 4 <= len(new_password) <= 8:
+                user = Users(new_username, new_password)
+                self.check_userName.update(user)
         else:
-            print("This name is already registered in the system. Choose another name.")
+            print("This name is already registered in the system.")
+        return user
 
-        length_pass = len(new_password)
-        if 4 <= length_pass <= 8:
-            self.password = new_password
-        else:
-            print("This password is invalid. Please change password")
-            self.password = input()
-
-    def log_in(self, new_userName, new_password):
-        if new_userName in self.check_userName:
+    def log_in(self, new_username, new_password):
+        if new_username in self.check_userName:
             if new_password in self.check_userName:
-                Users.isConnected = True
+                user = self.check_userName[new_username]
+                user.isConnected = True
                 print("The user is in")
 
-    def log_out(self, new_userName):
-        if new_userName in self.check_userName:
-            Users.isConnected = False
+    def log_out(self, new_username):
+        if new_username in self.check_userName:
+            user = self.check_userName[new_username]
+            user.isConnected = False
             print("The user is out")
-
-
