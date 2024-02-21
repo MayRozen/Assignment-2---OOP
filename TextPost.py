06be8afea52eb8e4e3c5642466b743dc0d0d3880
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-21 23:45:46 +0200

    Update TextPost.py

--- TextPost.py
+++ TextPost.py
@@ -1,10 +1,11 @@
-from Posts import Posts
+import Posts
 
 
 class TextPost(Posts):
     text = ""
 
     def __init__(self, text, owner, like_count, comments):
+        super().__init__()
         self.text = text
         super().__init__(owner, like_count, comments)
 
