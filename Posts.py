da7f961bbabe5003bed00b5a160ba7ade05c6f00
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-21 23:44:33 +0200

    Update Posts.py

--- Posts.py
+++ Posts.py
@@ -1,11 +1,7 @@
 from enum import Enum
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 
-from ImagePost import ImagePost
-from SalePost import SalePost
-from TextPost import TextPost
-from Users import Users
-
+import Users
 
 class PostType(Enum):
     TEXTPOST = "textPost"
@@ -18,37 +14,43 @@ class Posts:
     comments = []
     owner = None
 
-    def create_post(self, post_type, owner, *content):  # Factory
-        if owner.isConnected:
-            if post_type == post_type.TEXTPOST:
-                text = content[0]
-                print("Text Post")
-                return TextPost(text)
-            elif post_type == post_type.IMAGEPOST:
-                image = content[0]
-                print("Image Post")
-                return ImagePost(image)
-            elif post_type == post_type.SALEPOST:
-                description = content[0]
-                price = content[1]
-                location = content[2]
-                print("Sale Post")
-                return SalePost(description, price, location)
-            else:
-                raise ValueError("Invalid post type")
+    @staticmethod
+    def create_post(self, post_type, owner, *content):
+        self.owner = owner
+        if post_type == post_type.TEXTPOST:
+            text = content[0]
+            return TextPost(text)
+        elif post_type == post_type.IMAGEPOST:
+            image = content[0]
+            return ImagePost(image)
+        elif post_type == post_type.SALEPOST:
+            description = content[0]
+            price = content[1]
+            location = content[2]
+            return SalePost(description, price, location)
+        else:
+            raise ValueError("Invalid post type")
 
     def like(self, user):  # continue
         if self.owner.isConnected:
             self.like_count += 1
             if user != self.owner:
-                Users.notification.append(self.like_count)
+                for follower in self.owner.followers:
+                    self.owner.followers.updateLike(self.like_count)
 
     def comment(self, user, content):
         if self.owner.isConnected:
             self.comments.append(user, content)
             if user != self.owner:
-                Users.notification.append(self.like_count)
+                for follower in self.owner.followers:
+                    self.owner.followers.updateComment(content)
 
     @abstractmethod
     def print_info(self, user):
         pass
+
+
+    def notify(self, new_post):
+        for follower in self.owner.followers:
+            Users.followers.updatePost(new_post)
+
