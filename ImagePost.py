c02793cec7eb737f6a8a8fd1e06abab12435abf9
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-23 00:44:57 +0200

    Update ImagePost.py

--- ImagePost.py
+++ ImagePost.py
@@ -1,18 +1,19 @@
-import Posts
+from Posts import Posts
+import matplotlib.pyplot as plt
 
 
 class ImagePost(Posts):
-    image = ""
 
-    def __init__(self, image, owner, like_count, comments):
-        super().__init__()
-        self.image = image
-        super().__init__(owner, like_count, comments)
+    def __init__(self, user, image_post):
+        super().__init__(user)
+        self.image_post = image_post
 
     def display(self):
-        display_image = Image.open(self.image)
-        display_image.show()
+        display_image = plt.imread(self.image_post)
+        plt.imshow(display_image)
+        plt.axis('off')
+        plt.show()
+        print("Shows picture")
 
     def print_info(self):
-        image_string = "imagePost"
-        print(f"{self.owner.name} {Posts.create_post(image_string, self.owner, self.image).display()}")
+        return f"{self._user.username()}"
