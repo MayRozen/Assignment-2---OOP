ce0e4777bb1f59c198dcbda9b3fad6c12a00de5c
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-23 14:33:45 +0200

    Add files via upload

---
+++ FactoryPosts.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from ImagePost import ImagePost
+from SalePost import SalePost
+from TextPost import TextPost
+
+
+class PostType(Enum):
+    TEXTPOST = 'Text'
+    IMAGEPOST = 'Image'
+    SALEPOST = 'Sale'
+
+class FactoryPosts:
+
+    @staticmethod
+    def created_post(post_type, user, *content):
+        if post_type == PostType.TEXTPOST:
+            new_post = TextPost(user, *content)
+            return new_post
+        elif post_type == PostType.IMAGEPOST:
+            new_post = ImagePost(user, *content)
+            return new_post
+        elif post_type == PostType.SALEPOST:
+            new_post = SalePost(user, *content)
+            return new_post
+        else:
+            raise ValueError("Invalid post type")
