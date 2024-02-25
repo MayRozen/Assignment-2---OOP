e4b200e77509d0906aab59d4b3552674ef677dd7
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-25 17:24:56 +0200

    end ImagePost.py

--- ImagePost.py
+++ ImagePost.py
@@ -6,15 +6,15 @@ class ImagePost(Posts):
 
     def __init__(self, user, image_post):
         super().__init__(user)
-        self.image_post = image_post
+        self._image_post = image_post
 
     def display(self):
-        display_image = plt.imread(self.image_post)
+        display_image = plt.imread(self._image_post)
         plt.imshow(display_image)
         plt.axis('off')
         plt.show()
         print("Shows picture")
 
-    def print_info(self):
-        return f"{self._user.username()}"
-        
+    def __str__(self):
+        a = f"{self._owner.username()} posted a picture\n"
+        return a
