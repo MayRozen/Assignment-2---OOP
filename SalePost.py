9de93f74706d2228195f069c1af0db61abc453d3
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-02-25 17:26:41 +0200

    end SalePost.py

--- SalePost.py
+++ SalePost.py
@@ -5,28 +5,34 @@ class SalePost(Posts):
 
     def __init__(self, user, description, price, location):
         super().__init__(user)
-        self.description = description
-        self.price = price
-        self.location = location
-        self.available = True
+        self._description = description
+        self._price = price
+        self._location = location
+        self._available = True
 
-    @staticmethod
     def discount(self, percentage, password):
-        if self.user.password == password:
+        if self._owner.password() == password:
             if 0 <= percentage <= 100:
-                discount_amount = self.price * (percentage / 100)
-                discounted_price = self.price - discount_amount
-                self.price = discounted_price
-                print(f"new price after discount is {self.price}")
+                discount_amount = self._price * (percentage / 100)
+                discounted_price = self._price - discount_amount
+                self._price = discounted_price
+                print(f"Discount on {self._owner.username()} product! the new price is: {self._price}")
             else:
                 print("Invalid discount percentage. It should be between 0 and 100.")
 
     def sold(self, password):
-        if self._user.password == password:
-            self.available = False
+        if self._owner.password() == password:
+            self._available = False
+            print(f"\n{self._owner.username()}'s product is sold")
+            self.__str__()
 
-    def print_info(self):
-        if self.available:
-            print(f"it's {self.available} that the product of {self._user.name} is not sold")
+
+    def __str__(self):
+        if self._available:
+            text = "For sale!"
         else:
-            print(f"it's {self.available} that the product of {self._user.name} is sold")
+            text = "Sold!"
+
+        a = f"{self._owner.username()} posted a product for sale:\n"
+        b = f"{text} {self._description}, price: {self._price}, pickup from: {self._location}\n"
+        return a+b
